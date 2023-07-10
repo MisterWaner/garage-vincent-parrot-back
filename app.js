11b7523efa75d2c684bdc6aa5704b69aa34f0062
Erwan Soulas <erwan.soulas@gmail.com>
2023-07-10 23:31:16 +0200

    finsh connection between sequelize and db. Start models

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 /********************** Import modules *********************/
 import express from "express";
 import { config } from "dotenv";
+import db from "./config/sequelize-config.js";
 
 config();
 const app = express();
 
-
-
-
 /************************* Routes ************************/
 app.get("/api", (req, res) => {
-    res.send("API en ligne et fonctionnelle")
+    res.send("API en ligne et fonctionnelle");
 });
-
 
 /*************************** Start Server *************************/
-app.listen(process.env.SERVER_PORT, () => {
-    console.log(`Server is running on port ${process.env.SERVER_PORT}`);
-});
+
+db.sequelize
+    .authenticate()
+    .then(() => console.log("DB is connected"))
+    .then(() => {
+        app.listen(process.env.SERVER_PORT, () => {
+            console.log(`Server is running on port ${process.env.SERVER_PORT}`);
+        });
+    })
+    .catch((err) => console.log("error occured :", err));
