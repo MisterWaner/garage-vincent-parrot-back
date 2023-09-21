e565255b19233f31ba22898cacae5e6725a8ed9c
Erwan Soulas <erwan.soulas@gmail.com>
2023-09-21 20:19:07 +0200

    new attempt 3

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ import {fileURLToPath} from "url";
 
 config();
 const app = express();
-const port = process.env.SERVER_PORT || 3000;
+const port = process.env.PORT || 3000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
